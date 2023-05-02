4d2daf39b8e56e80c0e88a835eb8b4110cc81c5a
Ruslan442 <pruslan442@gmail.com>
2023-05-02 15:51:21 +0300

    ffff

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+    <header>
+        <img src="/img/poli.png">
+        <h1>Домашняя работа: Hello, World!</h1>
+    </header>
+
+    <body> 
+        <?='Hello WORLD!'?>
+    </body>
+    
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
