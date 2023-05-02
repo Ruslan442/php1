63a1d50ae3089cef6eb21a445ebba5fb72c0ee55
Ruslan442 <pruslan442@gmail.com>
2023-05-02 19:09:38 +0300

    end

--- index.php
+++ index.php
@@ -5,21 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
+    <link rel="stylesheet" href="/css/style.css"/>
 </head>
 <body>
 
     <header>
-        <img src="/img/poli.png">
+        <img src="/img/poli.png" class="logo">
         <h1>Домашняя работа: Hello, World!</h1>
     </header>
 
-    <body> 
+    <main> 
         <?='Hello WORLD!'?>
-    </body>
+</main>
     
 
     <footer>
-        
+        <p>задание для самостоятельно работы (без описания)</p>
     </footer>
 </body>
 </html>
